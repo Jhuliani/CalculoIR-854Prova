413ad00667cce2d9a91a07fa63491349953aa6ff
Jhuliani Cristina <jhulianisantos@gmail.com>
2022-07-12 13:52:03 -0300

    Projeto criado

---
+++ CalculoIR-854Prova/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CalculoIR_854Prova
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
